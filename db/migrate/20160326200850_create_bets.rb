4dc015560007a8e27c9ca43d11d70248140e2b7b
Krystian <krystian.guzara@gmail.com>
2016-04-09 12:38:22 +0200

    heroku config

--- db/migrate/20160326200850_create_bets.rb
+++ db/migrate/20160326200850_create_bets.rb
@@ -7,8 +7,8 @@ class CreateBets < ActiveRecord::Migration
       t.attachment :proof
       t.boolean :active
       t.datetime :end_date_of_challenge
-      t.references :user_owner, index: true, foreign_key: true
-      t.references :user_participant, index: true, foreign_key: true
+      t.references :user_owner, index: true
+      t.references :user_participant, index: true
 
       t.timestamps null: false
     end
--- db/migrate/20160329171448_add_columns_to_bet.rb
+++ db/migrate/20160329171448_add_columns_to_bet.rb
@@ -2,7 +2,7 @@ class AddColumnsToBet < ActiveRecord::Migration
   def change
     add_column :bets, :invitation, :string
     add_column :bets, :video_url, :string
-    add_reference :bets, :user_winner, index: true, foreign_key: true
+    add_reference :bets, :user_winner, index: true
     add_column :bets, :status, :integer
   end
 end
